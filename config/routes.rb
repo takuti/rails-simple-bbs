b5678aa5cf1801a8112b0684d92afd67745cd725
takuti <k.takuti@gmail.com>
2015-06-21 00:07:51 +0900

    add auxilliary route

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'posts#index'
+  get 'posts' => 'posts#index'
   get 'posts/new' => 'posts#new'
   post 'posts' => 'posts#create'
 
