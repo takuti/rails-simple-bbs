bf742e238427e88fb60cc0f5f76d007b756ad568
takuti <k.takuti@gmail.com>
2015-06-21 13:12:03 +0900

    add route to /posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   root 'posts#index'
+  get 'posts' => 'posts#index'
   post 'posts' => 'posts#create'
 
   # Example of regular route:
