afc4e29e2a36779728d4c11d3c6c0cd99107b3ad
takuti <k.takuti@gmail.com>
2015-06-21 16:15:09 +0900

    add model validation test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test 'should not save post without both of name and comment' do
+    post = Post.new
+    post.email = 'foo@bar.com'
+    assert_not post.save
+  end
+
+  test 'should not save post without name' do
+    post = Post.new
+    post.email = 'foo@bar.com'
+    post.comment = 'hello'
+    assert_not post.save
+  end
+
+  test 'should not save post without comment' do
+    post = Post.new
+    post.name = 'Tom'
+    post.email = 'foo@bar.com'
+    assert_not post.save
+  end
+
+  test 'should save post without email' do
+    post = Post.new
+    post.name = 'Tom'
+    post.comment = 'hello'
+    assert post.save
+  end
+
+  test 'should not save post with invalid email' do
+    post = Post.new
+    post.name = 'Tom'
+    post.email = 'awesome.email'
+    post.comment = 'hello'
+    assert_not post.save
+  end
+
 end
